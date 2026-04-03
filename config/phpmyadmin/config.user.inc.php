3efc50aed6ae73a77e5eb05fca093c6234adf084
GabrielMarlon <gabrielcursos19@gmail.com>
2026-04-03 12:00:24 -0300

    Adiciona configuração de charset e collation para MySQL e phpMyAdmin

---
+++ config/phpmyadmin/config.user.inc.php
@@ -0,0 +1,7 @@
+<?php
+// Collation padrão na tela de login
+$cfg['DefaultConnectionCollation'] = 'utf8mb4_general_ci';
+
+// Charset e collation padrão no formulário "Criar banco de dados"
+$cfg['DefaultCharset']   = 'utf8mb4';
+$cfg['DefaultCollation'] = 'utf8mb4_general_ci';
